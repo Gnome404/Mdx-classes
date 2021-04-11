6751e6bf92befe651be7a18f47fcb7c1265d2712
Gnome404 <Alliraheem3532@gmail.com>
2021-04-11 16:24:12 +0400

    First commit

---
+++ product.js
@@ -0,0 +1,51 @@
+let products = [{
+        id: 111,
+        title: "Agric",
+        description: "Let the kids learn the art of <em> irresistible </em>," +
+            "agriculture as a whole",
+        price: "$3.99",
+        image: 'image/agric.jpg',
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 112,
+        title: "Swimming",
+        description: "Swimming helps the kids <em> irresistible </em>," +
+            "to cool off and be in a relaxed state.",
+        price: "$2.99",
+        image: 'image/swim.jpg',
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 113,
+        title: "Sports",
+        description: "Sports helps to of <em> irresistible </em>," +
+            "shake off fatigue.",
+        price: "$2.50",
+        image: 'image/sports.jpg',
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 114,
+        title: "Martial Arts",
+        description: "Self defense is  <em> irresistible </em>," +
+            "useful in our students day to day activities.",
+        price: "$3.99",
+        image: 'image/martial.jpg',
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 115,
+        title: "Music",
+        description: "Music is a way of <em> irresistible </em>," +
+            "Expressing our in depth feelings.",
+        price: "$3.50",
+        image: 'image/music.jpg',
+        availableInventory: 10,
+        rating: 4
+    },
+]
